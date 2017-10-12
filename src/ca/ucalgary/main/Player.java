5bb7458399b40f6ee4f28ee1dfb0a87ba2f281ec
Bonspiel2 <cole_adams@shaw.ca>
2017-10-12 15:42:39 -0600

    Built player class

--- src/ca/ucalgary/main/Player.java
+++ src/ca/ucalgary/main/Player.java
@@ -1,5 +1,52 @@
 package ca.ucalgary.main;
 
 public class Player {
-		//hi
+		private int x;
+		private int y;
+		
+		private int health;
+		private int score;
+		
+		private String ship;
+		
+		public Player(int x, int y, int health) {
+			this.x = x;
+			this.y = y;
+			
+			this.health = health;
+			this.score = 0;
+			
+			this.ship = "A";
+		}
+		
+		public void move(String[][] board, String s) {
+			 if (s.equals("A")) {
+				 if (x - 1 >= 0) {
+					 x--;
+				 }
+			 } else if (s.equals("D")) {
+				 if (x + 1 >= board[0].length) {
+					 x++;
+				 }
+			 }
+		}
+		
+		public boolean collidedWith(Collectable c) {
+			boolean collided = false;
+			int cx = c.getX();
+			int cy = c.getY();
+			
+			if (cx == x && cy == y) {
+				score++;
+				collided = true;
+			}
+		}
+		
+		public Projectile shoot() {
+			return new Projectile(x, y-1);
+		}
+		
+		public String draw() {
+			return ship;
+		}
 }
