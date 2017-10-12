c20231f5d84b5b22fe66ef7e8d9aeb384ef05d0c
quinn.goddard <quinn.goddard@zone50-ec.cs.ucalgary.ca>
2017-10-12 15:52:40 -0600

    Updated projectile class

--- src/ca/ucalgary/main/Projectile.java
+++ src/ca/ucalgary/main/Projectile.java
@@ -1,5 +1,44 @@
 package ca.ucalgary.main;
 
 public class Projectile {
+	
+	private int x;
+	private int y;
+	private boolean collided;
+	private boolean edgy;
+	
+	// detects whether projectile at same coordinate as enemy
+	public boolean collidedWith(Enemy enemy) {
+		if(x == enemy.x && y == enemy.y) {
+			collided = true;
+		} else {
+			collided = false;
+		}
+		return collided;
+	}
+	// constructor
+	public Projectile(int newX, int newY){
+		x = newX;
+		y = newY; 
+		
+	}
+	// moves the projectile up one row and detects whether it has reached
+	// the edge of the board
+	public boolean move(String[][] board) {
+		// detects whether the projectile's current row is the same as the edge of the board
+		if(y == 0) {
+			edgy = true;
+		} else {
+			edgy = false;
+			y = y - 1;
+		}
+		return edgy;
+	}
+	// draws the projectile
+	public String draw() {
+		return "|";
+	}
+	
+	
 
 }
