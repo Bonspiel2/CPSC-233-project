ba870a9baca4c010adad5365cdec533c187acf23
QuinnGoddard <qnn.gddrd@gmail.com>
2017-10-15 09:04:30 -0600

    Updated work log.

--- src/ca/ucalgary/main/Main.java
+++ src/ca/ucalgary/main/Main.java
@@ -2,6 +2,8 @@ package ca.ucalgary.main;
 
 public class Main {
 	public static void main(String[] args) {
+        System.out.print("Text-Based Space Invaders \nHit enter to start. Control your ship using WASD.");
+
 		TextGame game = new TextGame();
         game.initBoard();
 		game.run();
