81c28cd58654658f59b2ec3dbc60b0806fd865dc
QuinnGoddard <qnn.gddrd@gmail.com>
2017-10-16 17:33:47 -0600

    Added documentation for main class.

--- src/ca/ucalgary/main/Main.java
+++ src/ca/ucalgary/main/Main.java
@@ -1,5 +1,12 @@
 package ca.ucalgary.main;
 
+/**
+ * Main launches the game after prompting the user to play.
+ *
+ * @author Group 3
+ *
+ */
+
 public class Main {
 	public static void main(String[] args) {
         System.out.print("Text-Based Space Invaders \nHit enter to start. Control your ship using WASD.");
