aeb42fab47c743ec12c414d3343b3cc17d3d381f
lilypollreis <lilypollreis@gmail.com>
2017-10-26 12:24:06 -0600

    Main menu update

--- src/ca/ucalgary/main/Main.java
+++ src/ca/ucalgary/main/Main.java
@@ -1,5 +1,7 @@
 package ca.ucalgary.main;
 
+import java.util.Scanner;
+
 /**
  * Main launches the game after prompting the user to play.
  *
@@ -9,12 +11,22 @@ package ca.ucalgary.main;
 
 public class Main {
 	public static void main(String[] args) {
-        System.out.print("Text-Based Space Invaders \nHit enter to start. Control your ship using WASD.");
-		
+        Scanner in = new Scanner(System.in);
+		System.out.print("Type 1 to play the text game or 2 to play the gooey game.");
+        int gameChoice = in.nextInt();
         TextGame g = new TextGame();
-        GooeyInterface gui = new GooeyInterface(g);
+        
+        
+        if(gameChoice == 1){
+        	System.out.println("You chose the text game! Control your ship using WASD.");
+        	g.run();
+        }
+        else if (gameChoice == 2){
+        	System.out.println("You chose the gooey game! Control your ship using WASD.");
+        	GooeyInterface gui = new GooeyInterface(g);
+        }
 
         
-        g.run();
+      
 	}
 }
