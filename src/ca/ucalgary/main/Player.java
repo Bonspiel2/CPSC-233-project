14506471959ceec606953eca47c376e21b61fc47
Bonspiel2 <cole_adams@shaw.ca>
2017-10-30 14:57:07 -0600

    Added health bar

--- src/ca/ucalgary/main/Player.java
+++ src/ca/ucalgary/main/Player.java
@@ -13,222 +13,229 @@ import java.awt.Graphics;
  */
 public class Player {
 
-    private int x;
-    private int y;
-    private int width;
-    private int height;
-    
-    private int step;
-		
-    
-    private int maxX; //columns
-    private int maxY; //rows
-    
-    private int health;
-    private int score;
-		
-    private String ship;
-		
-    private double firerate;
-    private double fireCount;
-    private double fireTimer;
-		
-		
-		/**
-		 * Main constructor for textgame
-		 * @param x new player's column value
-		 * @param y new player's row value
-		 * @param health new player's total health
-		 */
-		public Player(int x, int y, int health) {
-			this.x = x;
-			this.y = y;
-			this.width = 0;
-			this.height = 0;
-			
-			this.health = health;
-			this.score = 0;
-			
-			this.ship = "A";
-            
-			firerate = 0.5;
-			fireCount = 6;
-			fireTimer = fireCount * firerate;
-		}
-		
-		/**
-		 * Main constructor for GUI game
-		 * @param x new player's column value
-		 * @param y new player's row value
-		 * @param width new player's width
-		 * @param height new player's height
-		 * @param health new player's total health
-		 */
-		public Player(int x, int y, int width, int height, int health) {
-			this.x = x;
-			this.y = y;
-			this.width = width;
-			this.height = height;
-			
-			this.health = health;
-			this.score = 0;
-			
-			this.ship = "A";
-			
-			firerate = 0.5;
-			fireCount = 100;
-			fireTimer = fireCount * firerate;
-		}
-		
-		
-		/**
-		 * Moves player either left or right while keeping it on the
-		 * screen
-		 * @param s Either "A" or "D" denoting left or right
-		 */
-		public void move(String s) {
-			 if (s.equals("A")) {
-				 if (x - step >= 0) {
-					 x-= step;
-				 }
-			 } else if (s.equals("D")) {
-				 if ((x + step) < maxX) {
-					 x+= step;
-				 }
-			 } else if (s.equals("W")) {
-				 if ((y - step) >= 0) {
-					 y-= step;
-				 }
-			 } else if (s.equals("S")) {
-				 if ((y + step) < maxY) {
-					 y+= step;
-				 }
-			 }
-		}
-		
-		/**
-		 * Checks to see if the player has collided with a given collectable.
-		 * @param c Object to check for collision
-		 * @return Returns true if the player and the collectable occupy the
-		 * same space
-		 */
-		public boolean collidedWith(Collectable c) {
-			boolean collided = false;
-			int cx = c.getX();
-			int cy = c.getY();
-			int cw = c.getW();
-			int ch = c.getH();
-			
-			if (cx + cw >= x && cx < (x + width) && cy >= y && cy < y + height ) {
-				score++;
-				collided = true;
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+
+	private int step;
+
+
+	private int maxX; //columns
+	private int maxY; //rows
+
+	private int health;
+	private int initialHealth;
+	private int score;
+
+	private String ship;
+
+	private double firerate;
+	private double fireCount;
+	private double fireTimer;
+
+
+	/**
+	 * Main constructor for textgame
+	 * @param x new player's column value
+	 * @param y new player's row value
+	 * @param health new player's total health
+	 */
+	public Player(int x, int y, int health) {
+		this.x = x;
+		this.y = y;
+		this.width = 0;
+		this.height = 0;
+
+		this.health = health;
+		this.initialHealth = health;
+		this.score = 0;
+
+		this.ship = "A";
+
+		firerate = 0.5;
+		fireCount = 6;
+		fireTimer = fireCount * firerate;
+	}
+
+	/**
+	 * Main constructor for GUI game
+	 * @param x new player's column value
+	 * @param y new player's row value
+	 * @param width new player's width
+	 * @param height new player's height
+	 * @param health new player's total health
+	 */
+	public Player(int x, int y, int width, int height, int health) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+
+		this.health = health;
+		this.initialHealth = health;
+		this.score = 0;
+
+		this.ship = "A";
+
+		firerate = 0.5;
+		fireCount = 100;
+		fireTimer = fireCount * firerate;
+	}
+
+
+	/**
+	 * Moves player either left or right while keeping it on the
+	 * screen
+	 * @param s Either "A" or "D" denoting left or right
+	 */
+	public void move(String s) {
+		if (s.equals("A")) {
+			if (x - step >= 0) {
+				x-= step;
 			}
-			
-			return collided;
-		}
-		
-		/**
-		 * Creates a projectile when the player's cooldown is at 0
-		 * @return the projectile object fired or null when nothing fired
-		 */
-		public PlayerProjectile shoot() {
-			PlayerProjectile newShot = null;
-			fireTimer--;
-			boolean gui = false;
-			if(width != 0) {
-				gui = true;
+		} else if (s.equals("D")) {
+			if ((x + step) < maxX) {
+				x+= step;
 			}
-			if (fireTimer <= 0) {
-				newShot = new PlayerProjectile(x+(width/2), y-1);
-				fireTimer = firerate * fireCount;
+		} else if (s.equals("W")) {
+			if ((y - step) >= 0) {
+				y-= step;
 			}
-			return newShot;
+		} else if (s.equals("S")) {
+			if ((y + step) < maxY) {
+				y+= step;
+			}
 		}
-		
-		/**
-		 * Draws ship character on the board
-		 * @param board The text board
-		 */
-		public void draw(String[][] board) {
-            board[y][x] = ship;
-		}
-    
-        /**
-         * Draws the projectile for the GUI Game.
-         * @param g the current GUI graphics object.
-         */
-        public void draw(Graphics g) {
-        	g.setColor(Color.CYAN);
-        	g.fillOval(x, y, width, height);
-        }
-		
-		/**
-		 * Gets the player's column value
-		 * @return player's column value
-		 */
-		public int getX() {
-			return x;
-		}
-		
-		/**
-		 * Sets the player's column value
-		 * @param x New column value
-		 */
-		public void setX(int x) {
-			this.x = x;
-		}
-		
-		/**
-		 * Gets the player's row value
-		 * @return Player's row value
-		 */
-		public int getY() {
-			return y;
+	}
+
+	/**
+	 * Checks to see if the player has collided with a given collectable.
+	 * @param c Object to check for collision
+	 * @return Returns true if the player and the collectable occupy the
+	 * same space
+	 */
+	public boolean collidedWith(Collectable c) {
+		boolean collided = false;
+		int cx = c.getX();
+		int cy = c.getY();
+		int cw = c.getW();
+		int ch = c.getH();
+
+		if (cx + cw >= x && cx < (x + width) && cy >= y && cy < y + height ) {
+			score++;
+			collided = true;
 		}
 
-		/**
-		 * Sets the player's row value
-		 * @param y New row value
-		 */
-		public void setY(int y) {
-			this.y = y;
-		}
-    
-		/**
-		 * Gets the player's health value
-		 * @return player's health value
-		 */
-        public int getHealth() {
-            return health;
-        }
-        
-        /**
-         * Sets the player's health value
-         * @param health New health value
-         */
-        public void setHealth(int health) {
-        	this.health = health;
-        }
+		return collided;
+	}
 
-        /**
-         * Gets the player's score value
-         * @return player's score value
-         */
-		public int getScore() {
-			return score;
+	/**
+	 * Creates a projectile when the player's cooldown is at 0
+	 * @return the projectile object fired or null when nothing fired
+	 */
+	public PlayerProjectile shoot() {
+		PlayerProjectile newShot = null;
+		fireTimer--;
+		boolean gui = false;
+		if(width != 0) {
+			gui = true;
 		}
-    
-    public void setMaxX(int max) {
-        this.maxX = max;
-    }
-    
-    public void setMaxY(int max) {
-        this.maxY = max;
-    }
-    
-    public void setStep(int step) {
-        this.step = step;
-    }
+		if (fireTimer <= 0) {
+			newShot = new PlayerProjectile(x+(width/2), y-1);
+			fireTimer = firerate * fireCount;
+		}
+		return newShot;
+	}
+
+	/**
+	 * Draws ship character on the board
+	 * @param board The text board
+	 */
+	public void draw(String[][] board) {
+		board[y][x] = ship;
+	}
+
+	/**
+	 * Draws the projectile for the GUI Game.
+	 * @param g the current GUI graphics object.
+	 */
+	public void draw(Graphics g) {
+		g.setColor(Color.CYAN);
+		g.fillOval(x, y, width, height);
+		g.setColor(Color.RED);
+		g.fillRect(x, y + height + 5, width, 5);
+		g.setColor(Color.GREEN);
+		g.fillRect(x, y + height + 5, (int) (width * ((double)health/(double)initialHealth)), 5);
+	}
+
+	/**
+	 * Gets the player's column value
+	 * @return player's column value
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * Sets the player's column value
+	 * @param x New column value
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * Gets the player's row value
+	 * @return Player's row value
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * Sets the player's row value
+	 * @param y New row value
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	/**
+	 * Gets the player's health value
+	 * @return player's health value
+	 */
+	public int getHealth() {
+		return health;
+	}
+
+	/**
+	 * Sets the player's health value
+	 * @param health New health value
+	 */
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	/**
+	 * Gets the player's score value
+	 * @return player's score value
+	 */
+	public int getScore() {
+		return score;
+	}
+
+	public void setMaxX(int max) {
+		this.maxX = max;
+	}
+
+	public void setMaxY(int max) {
+		this.maxY = max;
+	}
+
+	public void setStep(int step) {
+		this.step = step;
+	}
 
 	public int getWidth() {
 		return width;
